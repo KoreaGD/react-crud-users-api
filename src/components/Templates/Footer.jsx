d1398ada7b9167ccb2c5a607a96f00aac0a5f93e
Everton Trajano <evertontrajoli@gmail.com>
2020-08-12 00:10:24 -0300

    Adding Footer

--- src/components/Templates/Footer.jsx
+++ src/components/Templates/Footer.jsx
@@ -1,7 +1,10 @@
 import './Footer.css';
 import React from 'react';
 
-export default props => 
+export default props =>
     <footer className="footer">
-        Footer 
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> <span className="text-danger">3</span>verton</strong>
+        </span>
     </footer>
