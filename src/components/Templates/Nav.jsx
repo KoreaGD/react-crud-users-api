b08fb22a2bf56f028f8dde04e7a381a0be5724fe
Everton Trajano <evertontrajoli@gmail.com>
2020-08-12 00:18:58 -0300

    Adding NavBar

--- src/components/Templates/Nav.jsx
+++ src/components/Templates/Nav.jsx
@@ -3,5 +3,8 @@ import React from 'react';
 
 export default props => 
     <aside className="menu-area">
-        Nav Bar
+        <nav className="menu">
+            <a href="#"><i className="fa fa-home"></i> Início</a>
+            <a href="#/users"><i className="fa fa-users"></i> Usuários</a>
+        </nav>
     </aside>
